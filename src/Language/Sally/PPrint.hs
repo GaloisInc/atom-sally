da35fb234cf899c53715b9545fa9ea89a666c97a
Benjamin Jones <bjones@galois.com>
2016-09-28 18:23:17 -0700

    Eliminate shadowing of width

--- src/Language/Sally/PPrint.hs
+++ src/Language/Sally/PPrint.hs
@@ -1,3 +1,14 @@
+-- |
+-- Module      :  Language.Sally.PPrint
+-- Copyright   :  Benjamin Jones 2016
+-- License     :  BSD3
+--
+-- Maintainer  :  benjaminfjones@gmail.com
+-- Stability   :  experimental
+-- Portability :  unknown
+--
+-- Exports a pretty printer for the main Sally types
+--
 module Language.Sally.PPrint (
   pprintSystem
 ) where
@@ -8,6 +19,6 @@ import Text.PrettyPrint.Leijen.Text
 import Language.Sally.Types
 
 pprintSystem :: SallySystem -> TL.Text
-pprintSystem = displayT . renderPretty ribbon width . pretty
+pprintSystem = displayT . renderPretty ribbon wid . pretty
   where ribbon = 72 / 80 :: Float
-        width  = 80
+        wid    = 80
