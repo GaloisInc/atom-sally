5f48aa61743fbc2743c6f7cac2cbe9ac2c125b84
Benjamin Jones <bjones@galois.com>
2016-10-03 16:46:56 -0700

    Make test program emit .mcmt files for each example Atom

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,11 +2,17 @@
 
 module Main where
 
+import System.FilePath.Posix
+import System.IO
+
 import Language.Atom hiding (compile)
 
 import Language.Sally.Types
 import Language.Sally.Translation
 import Language.Sally.PPrint
+
+testDir :: FilePath
+testDir = "test"
 
 
 -- Test Atoms -----------------------------------------------------------
@@ -18,6 +24,18 @@ atom1 = atom "atom1" $ do
   cond $ (value x) <. 10
   x <== (value x) + 1
 
+atom2 :: Atom()
+atom2 = atom "atom2" $ do
+  f <- bool "flag" False
+
+  atom "alice" $ do
+    a <- bool "a" False
+    cond $ (value f)
+    a <== Const True
+
+  atom "bob" $ do
+    f <== Const True
+
 
 -- Main -----------------------------------------------------------------
 
@@ -26,15 +44,16 @@ putHeader = putStrLn (replicate 72 '-')
 testCompile :: String -> Atom () -> IO ()
 testCompile nm atom' = do
   tr <- compile (nameFromS nm) TrConfig atom'
-  putSystemLn tr
-  putHeader
+  let fname = testDir </> nm ++ ".mcmt"
+  withFile fname WriteMode (hPutSystem tr)
+  putStrLn ("compiled " ++ fname)
 
 -- | List of 'Atom's to translate and print
 suite :: [(String, Atom ())]
 suite = [ ("test_atom1", atom1)
+        , ("test_atom2", atom2)
         ]
 
 main :: IO ()
 main = do
-  putHeader
   mapM_ (uncurry testCompile) suite
