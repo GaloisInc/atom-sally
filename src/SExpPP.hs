3adff1d92070bdbca652253511a35abc72189b71
Benjamin Jones <bjones@galois.com>
2016-10-03 14:44:17 -0700

    Change s-expression printer to a simpler vertical aligned style

--- src/SExpPP.hs
+++ src/SExpPP.hs
@@ -1,15 +1,28 @@
+-- |
+-- Module      :  SExpPP
+-- Copyright   :  Benjamin F Jones 2016
+-- License     :  BSD3
+--
+-- Maintainer  :  bjones@galois.com
+-- Stability   :  experimental
+-- Portability :  unknown
+--
+-- This module gives a uniform way to pretty print s-expressions through a
+-- typeclass 'ToSExp'.
+--
+
 module SExpPP (
     SExp(..)
   , ToSExp(..)
   , bareText
 ) where
 
-
 import Data.Text.Lazy (Text)
 import Text.PrettyPrint.Leijen.Text
 
-hang' :: Doc -> Doc
-hang' =  hang 2
+
+-- hang' :: Doc -> Doc
+-- hang' =  hang 2
 
 data SExp = SXBare Doc     -- ^ bare symbol or literal
           | SXList [SExp]  -- ^ (foo a b)
@@ -26,9 +39,10 @@ instance ToSExp SExp where
 sxPrettyDefault :: SExp -> Doc
 sxPrettyDefault (SXBare x) = x
 sxPrettyDefault (SXList []) = lparen <> rparen
-sxPrettyDefault (SXList ll@(x:_)) = case x of
-  SXBare _ -> parens (hang' (fillSep (map sxPretty ll)))
-  SXList _ -> parens (fillSep (map sxPretty ll))
+sxPrettyDefault (SXList xs) = parens (align (vcat (fmap sxPretty xs)))
+-- sxPrettyDefault (SXList ll@(x:_)) = case x of
+--   SXBare _ -> parens (hang' (fillSep (map sxPretty ll)))
+--   SXList _ -> parens (fillSep (map sxPretty ll))
 
 bareText :: Text -> SExp
 bareText = SXBare . text
