0b55443166a55ce7e532a5cce6c4b7f6c33f29a7
Benjamin Jones <bjones@galois.com>
2016-10-04 15:18:16 -0700

    Add a third Atom example that uses channels
    Note: Sally doesn't seem to converge when given the property/query
    specified in Spec.hs for it.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Data.Int
 import System.FilePath.Posix
 import System.IO
 
@@ -18,12 +19,15 @@ testDir = "test"
 -- Test Atoms -----------------------------------------------------------
 
 -- | 'x' starts at 0, increases each tick up to 10
+--   Property: G(atom1!x >= 0)
 atom1 :: Atom ()
 atom1 = atom "atom1" $ do
   x <- int8  "x" 0
   cond $ (value x) <. 10
   x <== (value x) + 1
 
+-- | Two Atoms communicate through a flag
+--   Property: G(atom2!alice!a => atom2!flag)
 atom2 :: Atom()
 atom2 = atom "atom2" $ do
   f <- bool "flag" False
@@ -35,6 +39,35 @@ atom2 = atom "atom2" $ do
 
   atom "bob" $ do
     f <== Const True
+
+type MsgType = Int64
+
+-- | Two Atoms communicate through a *channel*
+--   Property: G((/= atom3!bob!msg -1) => atom3!alice!done)
+--             F((/= atom3!bob!msg -1))
+atom3 :: Atom()
+atom3 = atom "atom3" $ do
+
+  let
+    -- | Special message value indicating "no message present"
+    missingMsgValue :: MsgType
+    missingMsgValue = (-1)
+
+    -- | Special message value indicating "correct (intended) message"
+    goodMsg :: E MsgType
+    goodMsg = Const 1
+
+  (cin, cout) <- channel "aTob" missingMsgValue
+
+  atom "alice" $ do
+    done <- bool "done" False
+    writeChannel cin goodMsg
+    done <== Const True
+
+  atom "bob" $ do
+    msg <- int64 "msg" missingMsgValue
+    condChannel cout
+    msg <== readChannel cout
 
 
 -- Main -----------------------------------------------------------------
@@ -58,6 +91,9 @@ suite =
         "(query A1_transition_system (<= 0 A1!atom1!x))")
   , ("A2", atom2,
         "(query A2_transition_system (=> A2!atom2!alice!a A2!atom2!flag))")
+  , ("A3", atom3,
+        unwords [ "(query A3_transition_system"
+                , "  (=> (not (= A3!atom3!bob!msg (-1))) A3!atom3!alice!done))"])
   ]
 
 main :: IO ()
